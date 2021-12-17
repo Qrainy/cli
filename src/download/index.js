3ebc23587e2fffc683f118a79b9aabcd989e3e6e
Rainy <422118110@qq.com>
2021-12-17 20:54:04 +0800

    fix:  git clone error

--- src/download/index.js
+++ src/download/index.js
@@ -1,15 +1,26 @@
 const download = require('download-git-repo')
 
-const gitHubUrl = 'direct:git@github.com:Qrainy/cli-vue-demo.git#master'
+const gitHubUrl = 'https://github.com/Qrainy/cli-vue-demo.git'
+const gitHubUrlSpare = 'direct:git@github.com:Qrainy/cli-vue-demo.git#master'
 
-function main(projectName) {
+function main(name) {
   return new Promise((resolve, reject) => {
-    download(gitHubUrl, projectName || 'vue-demo', { clone: true }, function (err) {
-      if (err) {
-        reject(err)
-      }
-      resolve('模板拉取成功~')
-    })
+   
+    downloadDemo(gitHubUrl)
+
+    function downloadDemo(url, isEnd = false) {
+      download(url, name, { clone: true }, function (err) {
+        if (err) {
+          if (isEnd) {
+            reject(err)
+          } else {
+            // 下载备用地址
+            downloadDemo(gitHubUrlSpare, true)
+          }
+        }
+        resolve('模板拉取成功~')
+      })
+    }
   })
 }
 
