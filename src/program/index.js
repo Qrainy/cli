73d7e104cce6b5d2fa8e5bf8192c73130dbe3c33
Rainy <422118110@qq.com>
2021-12-19 19:27:14 +0800

    test: command add

--- src/program/index.js
+++ src/program/index.js
@@ -3,24 +3,36 @@ const { version, showVersion, showLogo, showStartUp } = require('../utils')
 const { createMain } = require('./create')
 
 function main() {
+  commandCreate()
+  commandAdd()
+  program.parse(process.argv)
+}
+
+function commandCreate() {
   program
     .version(version)
     .command('create <name>')
     .description('create a new project')
     .action(async (name) => {
-      const create = new createMain(name)
-
       showVersion()
-
+      const create = new createMain(name)
       const status = await create.init()
-
       if (status) {
         showStartUp(name)
         showLogo()
       }
     })
+}
 
-  program.parse(process.argv)
+function commandAdd() {
+  program
+    .command('add <plugin> [pluginOptions]')
+    .description('install a plugin and invoke its generator in an already created project')
+    .option('--registry <url>', 'Use specified npm registry when installing dependencies (only for npm)')
+    .allowUnknownOption()
+    .action((plugin) => {
+      console.log('===', plugin)
+    })
 }
 
 module.exports = main
